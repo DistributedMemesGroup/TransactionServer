b6b6e99dc4331c0247f683c9e11930c7c067885a
Andrew Munoz <43388560+am009168@users.noreply.github.com>
2021-03-27 08:15:12 -0700

    Update Account.java

--- src/account/Account.java
+++ src/account/Account.java
@@ -1,7 +1,7 @@
 package account;
 
 import concurrency.locking.Lock;
-
+//init Account
 public class Account {
     private static int idcount = 0;
 
@@ -10,13 +10,14 @@ public class Account {
     private int balance;
     // Lock tied to this account
     public Lock lock;
-
+    
+    //constructor for account
     public Account(int input) {
         balance = input;
         id = idcount;
         idcount++;
     }
-
+    
     public int getBalance() {
         return balance;
     }
@@ -24,12 +25,14 @@ public class Account {
     public void writeBalance(int value) {
         balance = value;
     }
-
+    
+    //check if account is equal to another account
     @Override
     public boolean equals(Object other) {
         return other instanceof Account && ((Account) other).id == this.id;
     }
-
+    
+    //get account ID
     public int getId() {
         return this.id;
     }
