fe7a6e49e3d46e0849ba315b188d2c403ed16c64
Andrew Munoz <43388560+am009168@users.noreply.github.com>
2021-03-27 08:50:28 -0700

    Update TransactionManager.java

--- src/concurrency/transaction/TransactionManager.java
+++ src/concurrency/transaction/TransactionManager.java
@@ -10,21 +10,23 @@ import logger.Logger;
 import utils.TransactionConnection;
 
 public class TransactionManager {
+    //init variables
     private Hashtable<Integer, Transaction> transactions = new Hashtable<>();
     private static TransactionManager instance = null;
     private static final LockManager lockManager = LockManager.getInstance();
     private static int currentId = 0;
     private final Logger logger = Logger.getInstance();
-
+    
+    //Get current instance of transaction
     public static TransactionManager getInstance() {
         if (instance == null) {
             instance = new TransactionManager();
         }
         return instance;
     }
-
+    
+     // Spawn the transacmanagerworkerthread with conn
     public void handleConnection(Socket socket) {
-        // Spawn the transacmanagerworkerthread with conn
         try {
             var conn = new TransactionConnection(socket);
             Thread transacWorkerThread = new Thread(new TransactionManagerWorker(conn));
@@ -35,7 +37,8 @@ public class TransactionManager {
         }
 
     }
-
+    
+    //Add a transaction to out hash map
     public synchronized int addTransaction() {
         Transaction newTrans = new Transaction(currentId);
         // Put the transaction in the hashtable with its id.
@@ -44,7 +47,8 @@ public class TransactionManager {
         currentId++;
         return newTrans.transactionID;
     }
-
+    
+    //Get information of the desired transaction
     public synchronized Transaction getTransaction(int transactionId) {
         if (transactions.containsKey(transactionId)) {
             return transactions.get(transactionId);
@@ -52,7 +56,8 @@ public class TransactionManager {
             return null;
         }
     }
-
+    
+    //Remove transaction from the map
     public synchronized void removeTransaction(int transactionId) {
         var transaction = transactions.remove(transactionId);
         lockManager.unlock(transaction);
