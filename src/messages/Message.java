edd231682c5ad6dcbaa884f6fcf198cbe27d8687
Andrew Munoz <43388560+am009168@users.noreply.github.com>
2021-03-27 08:55:14 -0700

    Update Message.java

--- src/messages/Message.java
+++ src/messages/Message.java
@@ -2,14 +2,17 @@ package messages;
 
 import java.io.Serializable;
 
+//Create message class
 public class Message implements Serializable {
     static final long serialVersionUID = 420l;
     MessageType type;
-
+   
+    //Set message type
     public Message(MessageType type) {
         this.type = type;
     }
-
+    
+    //Get message type
     public MessageType getType() {
         return type;
     }
